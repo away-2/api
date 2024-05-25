b59866210d0f6f74f8f264ceec003997e77c62be
away-2@1968642178@qq.com <1968642178@qq.com>
2024-05-25 18:04:48 +0800

    错误处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser } = require('../service/user.service')
+const { createUser, getUserInfo } = require('../service/user.service')
 
 class UserController {
     // 用户注册
@@ -6,6 +6,27 @@ class UserController {
         // 1. 获取数据
         // console.log(ctx.request.body);
         const { user_name, password } = ctx.request.body
+        // 合法性
+        if(!user_name || !password) {
+            console.error('用户名或密码为空', ctx.request.body)
+            ctx.status = 400
+            ctx.body = {
+                code: '10001',
+                message: '用户名或密码为空',
+                result: ''
+            }
+            return
+        }
+        // 合理性
+        if(getUserInfo({ user_name })) {
+            ctx.status = 409
+            ctx.body = {
+                code: '10002',
+                message: '用户已存在',
+                result: ''
+            }
+            return
+        }
         // 2. 操作数据库
         const res = await createUser(user_name, password)
         console.log(res);
